f95fca8ec4dbe5b91630f3ec1141432a18723afa
joaoibira-ux <joao.ibira@gmail.com>
2026-06-04 18:22:39 -0300

    gw v1.6: botao Menu GW movido para baixo ao centro (nao sobrepoe apps)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "gw-v9";
+const VERSION = "gw-v10";
 const ASSETS = ["./index.html", "./instalar.html", "./Logo-gw.png", "./manifest.json"];
 
 self.addEventListener("install", e => {
