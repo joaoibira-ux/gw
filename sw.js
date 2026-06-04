d296a3b173ab9b15003bf0d05610044ce1f19a41
joaoibira-ux <joao.ibira@gmail.com>
2026-06-04 18:49:49 -0300

    gw v1.9: scope / no manifest, navegacao direta sem iframe sem barra

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "gw-v12";
+const VERSION = "gw-v13";
 const ASSETS = ["./index.html", "./instalar.html", "./Logo-gw.png", "./manifest.json"];
 
 self.addEventListener("install", e => {
