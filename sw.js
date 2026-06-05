dc52252f34ec1ccf73245a3a2fd70490c0d6d4b0
joaoibira-ux <joao.ibira@gmail.com>
2026-06-04 21:05:59 -0300

    gw v2.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "gw-v19";
+const VERSION = "gw-v20";
 const ASSETS = ["./index.html", "./instalar.html", "./Logo-gw.png", "./manifest.json"];
 
 self.addEventListener("install", e => {
