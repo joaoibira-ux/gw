7a3469b45325c178e3f1923826970e3dbb5194cc
joaoibira-ux <joao.ibira@gmail.com>
2026-06-04 21:18:04 -0300

    gw v2.8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "gw-v21";
+const VERSION = "gw-v22";
 const ASSETS = ["./index.html", "./instalar.html", "./Logo-gw.png", "./manifest.json"];
 
 self.addEventListener("install", e => {
