b2824b6d834c783c1d17413353b73ca14a875d6e
joaoibira-ux <joao.ibira@gmail.com>
2026-06-05 07:01:39 -0300

    gw v3.2: pin 3733 oculta Caixa e Relatorio (modo restrito)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "gw-v25";
+const VERSION = "gw-v26";
 const ASSETS = ["./index.html", "./instalar.html", "./Logo-gw.png", "./manifest.json"];
 
 self.addEventListener("install", e => {
