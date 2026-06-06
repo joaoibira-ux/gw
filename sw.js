b1649972c9132f401e23bb6d44af8cc567ebfb57
joaoibira-ux <joao.ibira@gmail.com>
2026-06-06 18:31:28 -0300

    sistema v3.9 (gw-v32): bump apos atualizacao folha v4.54 fundo verde

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "gw-v31";
+const VERSION = "gw-v32";
 const ASSETS = ["./index.html", "./instalar.html", "./Logo-gw.png", "./manifest.json", "./instrucoes_sistema_gw.png", "./Aviso iPhone.png", "./Aviso Adroide.png"];
 
 self.addEventListener("install", e => {
